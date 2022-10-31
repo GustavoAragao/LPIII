dafc74f28d53b7170e06dbd9648816cfff7bb674
GustavoAragao <116197274+GustavoAragao@users.noreply.github.com>
2022-10-30 23:11:31 -0300

    Add files via upload

---
+++ data/Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    Data data = new Data();
+
+    data.setData(22, 2, 2022);
+    data.mostraData();
+    data.mostraDataExtenso();
+    System.out.println("Tá funfano! :)");
+  }
+}
